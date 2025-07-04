f2adb40713a75576f633291d2375ca17a03b83c0
chidimez <chidi_mez@yahoo.com>
2025-07-04 01:31:07 +0100

    Create Post model and migration with scheduling and publishing support

---
+++ app/Models/Post.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'content',
+        'author',
+        'scheduled_at',
+        'published_at',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'scheduled_at' => 'datetime',
+        'published_at' => 'datetime',
+    ];
+}
